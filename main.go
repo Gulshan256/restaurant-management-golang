04798b6e7eac46a35def5713d2df38984b20dff3
Gulshan256 <kumargk887@gmail.com>
2024-04-15 10:56:24 +0530

    add swagger into application

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/devGulshan/restaurant-management/initializers"
 	"github.com/devGulshan/restaurant-management/middleware"
 	"github.com/devGulshan/restaurant-management/routes"
+
+	"github.com/devGulshan/restaurant-management/docs"
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
 func init() {
@@ -15,6 +19,16 @@ func init() {
 
 	initializers.ConnectToDb() // Initialize the database connection
 }
+
+// @title Restaurant Management API
+// @version 1
+// @description This is a simple restaurant management API
+// @contact.name Gulshan Kumar
+// @contact.url http://www.gulshankumar.com
+// @contact.email https://www.gulshankumar.com
+// @BasePath /api/v1
+// @schemes http
+// @host localhost:8080
 
 func main() {
 
@@ -25,8 +39,12 @@ func main() {
 
 	router := gin.Default()
 	// router := gin.New()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler)) // http://localhost:8080/swagger/index.html
 	router.Use(gin.Logger())
+
 	routes.UserRouter(router)
+
 	router.Use(middleware.Authentication())
 
 	routes.FoodRoutes(router)
@@ -36,6 +54,9 @@ func main() {
 	routes.OrderItemsRoutes(router)
 	routes.InvoiceRoutes(router)
 
-	router.Run(":" + port)
+	err := router.Run(":" + port)
+	if err != nil {
+		panic(err)
+	}
 
 }
